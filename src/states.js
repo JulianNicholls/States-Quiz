858d3156344ad382bc6de444af72391af624cddb
Julian Nicholls <juliannicholls29@gmail.com>
2019-03-10 21:45:20 +0000

    Set up basic app

---
+++ src/states.js
@@ -0,0 +1,202 @@
+export default [
+  {
+    name: 'Washington',
+    capital: 'Olympus',
+  },
+  {
+    name: 'Oregon',
+    capital: 'Salem',
+  },
+  {
+    name: 'California',
+    capital: 'Sacramento',
+  },
+  {
+    name: 'Idaho',
+    capital: 'Boise',
+  },
+  {
+    name: 'Nevada',
+    capital: 'Carson City',
+  },
+  {
+    name: 'Arizona',
+    capital: 'Phoenix',
+  },
+  {
+    name: 'Montana',
+    capital: 'Helena',
+  },
+  {
+    name: 'Wyoming',
+    capital: 'Cheyenne',
+  },
+  {
+    name: 'Utah',
+    capital: 'Salt Lake City',
+  },
+  {
+    name: 'Colorado',
+    capital: 'Denver',
+  },
+  {
+    name: 'New Mexico',
+    capital: 'Santa Fe',
+  },
+  {
+    name: 'North Dakota',
+    capital: 'Bismarck',
+  },
+  {
+    name: 'South Dakota',
+    capital: 'Pierre',
+  },
+  {
+    name: 'Nebraska',
+    capital: 'Lincoln',
+  },
+  {
+    name: 'Kansas',
+    capital: 'Topeka',
+  },
+  {
+    name: 'Oklahoma',
+    capital: 'Oklahoma City',
+  },
+  {
+    name: 'Texas',
+    capital: 'Austin',
+  },
+  {
+    name: 'Minnesota',
+    capital: 'St Paul',
+  },
+  {
+    name: 'Iowa',
+    capital: 'Des Moines',
+  },
+  {
+    name: 'Missouri',
+    capital: 'Jefferson City',
+  },
+  {
+    name: 'Arkansas',
+    capital: 'Little Rock',
+  },
+  {
+    name: 'Louisiana',
+    capital: 'Baton Rouge',
+  },
+  {
+    name: 'Wisconsin',
+    capital: 'Madison',
+  },
+  {
+    name: 'Illinois',
+    capital: 'Springfield',
+  },
+  {
+    name: 'Michigan',
+    capital: 'Lansing',
+  },
+  {
+    name: 'Indiana',
+    capital: 'Indianapolis',
+  },
+  {
+    name: 'Kentucky',
+    capital: 'Frankfort',
+  },
+  {
+    name: 'Tennessee',
+    capital: 'Nashville',
+  },
+  {
+    name: 'Mississippi',
+    capital: 'Jackson',
+  },
+  {
+    name: 'Alabama',
+    capital: 'Montgomery',
+  },
+  {
+    name: 'Georgia',
+    capital: 'Atlanta',
+  },
+  {
+    name: 'Ohio',
+    capital: 'Columbus',
+  },
+  {
+    name: 'West Virginia',
+    capital: 'Charleston',
+  },
+  {
+    name: 'Virginia',
+    capital: 'Richmond',
+  },
+  {
+    name: 'North Carolina',
+    capital: 'Raleigh',
+  },
+  {
+    name: 'South Carolina',
+    capital: 'Columbia',
+  },
+  {
+    name: 'Florida',
+    capital: 'Tallahassee',
+  },
+  {
+    name: 'Maine',
+    capital: 'Augusta',
+  },
+  {
+    name: 'Vermont',
+    capital: 'Montpelier',
+  },
+  {
+    name: 'New Hampshire',
+    capital: 'Concord',
+  },
+  {
+    name: 'New York',
+    capital: 'Albany',
+  },
+  {
+    name: 'Massachusetts',
+    capital: 'Boston',
+  },
+  {
+    name: 'Rhode Island',
+    capital: 'Providence',
+  },
+  {
+    name: 'Connecticut',
+    capital: 'Hartford',
+  },
+  {
+    name: 'Pennsylvania',
+    capital: 'Hariisburg',
+  },
+  {
+    name: 'New Jersey',
+    capital: 'Trenton',
+  },
+  {
+    name: 'Delaware',
+    capital: 'Dover',
+  },
+  {
+    name: 'Maryland',
+    capital: 'Annapolis',
+  },
+  {
+    name: 'Alaska',
+    capital: 'Juneau',
+  },
+  {
+    name: 'Hawaii',
+    capital: 'Honolulu',
+  },
+];
