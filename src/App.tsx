dec28f94677e11252955ad78ab11cd44a79719ab
Julian Nicholls <juliannicholls29@gmail.com>
2020-05-11 18:38:02 +0100

    Add basic three phase components

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,39 @@ import React from 'react';
 import Quiz from './components/Quiz';
 
 import './App.css';
+import { useGameState, SELECTION, PLAYING, COMPLETE } from './context';
 
-function App() {
+const SelectGame = (): JSX.Element => {
+  const { setPhase } = useGameState();
+
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center' }}>Select game</h2>
+      <button onClick={() => setPhase(PLAYING)}>Go</button>
+    </div>
+  );
+};
+
+const GameComplete = (): JSX.Element => {
+  const { score } = useGameState();
+
+  return <h2 style={{ textAlign: 'center' }}>Score: {score}</h2>;
+};
+
+function App(): JSX.Element {
+  const { phase } = useGameState();
+
   return (
     <header>
       <h1>States Quiz</h1>
       <div className="container">
-        <Quiz mapsrc="images/states.png" />
+        {phase === COMPLETE ? (
+          <GameComplete />
+        ) : phase === PLAYING ? (
+          <Quiz mapsrc="images/states.png" />
+        ) : (
+          <SelectGame />
+        )}
       </div>
     </header>
   );
